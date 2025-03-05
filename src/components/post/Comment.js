34c49c6068e14a940959646c9dd787e6eabb1571
Rui Zhao <rzhao7815@gmail.com>
2025-03-05 09:39:13 -0600

    Remove handlePress function

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { TouchableOpacity, View, Image } from "react-native";
+import { View } from "react-native";
 import { SvgUri } from "react-native-svg";
 import { commentIcon } from "../../utils/icons";
 
 const Comment = () => {
-    const handlePress = () => {
-        //handel comment
-    };
-
     return (
-        <TouchableOpacity onPress={handlePress}>
+        <View>
             <SvgUri 
                 uri={commentIcon}
                 width="16" 
                 height="14"  
             />
-        </TouchableOpacity>
+        </View>
     );
 }
 
