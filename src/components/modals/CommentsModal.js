c19a1f0b9bb5d887bdf8ab8898b53f18b3b283e9
Rui Zhao <rzhao7815@gmail.com>
2025-03-30 21:53:03 -0500

    Fix reply to a reply

--- src/components/modals/CommentsModal.js
+++ src/components/modals/CommentsModal.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { 
     Modal, 
     View, 
@@ -13,7 +13,8 @@ import {
     ActivityIndicator
  } from 'react-native';
 import baseUrl from '../../utils/api';
-import { cancelGradient, sendGradient } from '../../utils/icons';
+import Heart from '../post/Heart';
+import { cancelGradient, sendGradient, reply } from '../../utils/icons';
 import { SvgUri } from "react-native-svg";
 import apiClient from '../../services/apiClient';
 
@@ -21,7 +22,11 @@ const CommentsModal = ({ post, visible, onClose }) => {
     const [isLoading, setIsLoading] = useState(false);
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
-
+    const [replies, setReplies] = useState([]);
+    const [activeCommentId, setActiveCommentId] = useState(null);
+    const [newReply, setNewReply] = useState('');
+    const [selectedCommentId, setSelectedCommentId] = useState(null);
+    console.log('post',post.id);
     // Function to format time
     const formatTimestamp = (timestamp) => {
         const now = new Date();
@@ -49,30 +54,55 @@ const CommentsModal = ({ post, visible, onClose }) => {
         setIsLoading(true);
         try {
             const response = await apiClient.get(`${baseUrl}/comments/EV/${post.id}/`);
+            console.log('responsedata', response.data);
+            const transformedComments = response.data.map(comment => ({
+                id: comment?.id,
+                text: comment?.content,  
+                user: {
+                    avatar: `data:image/jpeg;base64, ${comment?.profilephoto_url}`,
+                    name: comment?.user,
+                },
+                timestamp: formatTimestamp(comment?.created_at),
 
-            const transformedComments = response.data.map(comment => {
-                return {
-                    text: comment?.content,  
+                replies: comment?.replies?.map(reply => ({
+                    id: reply?.id,
+                    text: reply?.content,
                     user: {
-                        avatar: `data:image/jpeg;base64, ${comment?.profilephoto_url}`,
-                        name: comment?.user,
+                        avatar: `data:image/jpeg;base64, ${reply?.profilephoto_url}`,
+                        name: reply?.user,
                     },
-                    timestamp: formatTimestamp(comment?.created_at),
-                };
-            });
-    
+                    timestamp: formatTimestamp(reply?.created_at),
+                    parentId: comment?.id
+                })) || []
+                
+            }));
+
+            const allReplies = response.data.flatMap(comment =>
+                comment?.replies?.map(reply => ({
+                    id: reply?.id,
+                    text: reply?.content,
+                    user: {
+                        avatar: `data:image/jpeg;base64, ${reply?.profilephoto_url}`,
+                        name: reply?.user,
+                    },
+                    timestamp: formatTimestamp(reply?.created_at),
+                    parentId: comment?.id
+                })) || []
+            );
+
             setComments(transformedComments);
+            setReplies(allReplies);
+            console.log('allreplies',allReplies);
         } catch (error) {
-            console.error('Error fetching comments:', error);
+            console.error('Error fetching comments and replies:', error);
         } finally {
             setIsLoading(false);
         }
     }, [post.id]);
 
     useEffect(() => {
-        if (post.id) {
-            fetchComments();
-        }
+        if (!post?.id) return;
+        fetchComments();
     }, [post.id, fetchComments]);
 
     // Handle add new comments
@@ -83,31 +113,194 @@ const CommentsModal = ({ post, visible, onClose }) => {
                 content_type: 'EV',
                 content_object_id: post.id,
             });
-
+           
             const newComentObj = {
                 text: newComment,
                 user: { avatar: post.profile_photo_url, name: post.username },
                 timestamp: formatTimestamp(new Date().toISOString()),
             };
 
-            setComments((prevComments) => [newComentObj, ...prevComments, ]);
+            setComments((prevComments) => [newComentObj, ...prevComments]);
             setNewComment('');
         } catch (error) {
             console.error('Error posting comment:', error);
         }
     };
+  
+    // Handle add new reply
+    const handleAddReply = async (parentId, replyText) => {
+        if (!replyText.trim()) return;
 
-    const renderComment = ({ item }) => (
-        <View style={styles.commentContainer}>
-            <Image source={{ uri: item.user.avatar }} style={styles.avatar} />
-            <View style={styles.commentContent}>
-                <Text style={styles.commentUser}>{item.user.name}</Text>
-                <Text style={styles.commentTimestamp}>{item.timestamp}</Text>
-                <Text style={styles.commentText}>{item.text}</Text>
-                
+        try {
+            await apiClient.post(`${baseUrl}/comments/`, {
+                parent: parentId,
+                content: replyText,
+                content_type: 'EV',
+                content_object_id: post.id,
+            });
+
+            const newReplyObj = {
+                text: replyText,
+                user: { avatar: post.profile_photo_url, name: post.username },
+                timestamp: formatTimestamp(new Date().toISOString()),
+                parentId: parentId,
+            };
+
+            setComments(prevComments =>
+                prevComments.map(comment =>
+                    comment.id === parentId
+                        ? { ...comment, replies: [...comment.replies, newReplyObj] }
+                        : comment
+                )
+            );
+    
+        } catch (error) {
+            console.error('Error posting Reply:', error);
+        }
+    };
+    
+    // Comm
+    const CommentItem = ({ item, comments }) => {
+        const [isReplying, setIsReplying] = useState(false);
+        const [showReplies, setShowReplies] = useState(false);
+        const [replyText, setReplyText] = useState('');
+        const replyInputRef = useRef(null);
+
+        const handleReplyPress= () => {
+            setIsReplying(true);
+            setTimeout(() => replyInputRef.current?.focus(), 100);
+        }
+        return (
+            <View style={styles.commentContainer}>
+                <Image source={{ uri: item.user.avatar }} style={styles.avatar} />
+                <View style={styles.commentContent}>
+                    <Text style={styles.commentUser}>{item.user.name}</Text>
+                    <Text style={styles.commentTimestamp}>{item.timestamp}</Text>
+                    <Text style={styles.commentText}>{item.text}</Text>
+                    <View style={styles.iconsContainer}>
+                        {/*like*/}
+                        <TouchableOpacity onPress={handleReplyPress}>
+                            <SvgUri uri={reply} width={14} height={14} />
+                        </TouchableOpacity>
+
+                        {/* Show replies button */}
+                        {item.replies.length > 0 && ( 
+                            <TouchableOpacity onPress={() => setShowReplies(prev => !prev)}>
+                                <Text style={styles.totalReplies}>
+                                    {showReplies ? "-- Hide replies" : `-- View ${item.replies.length} replies`}
+                                </Text>
+                            </TouchableOpacity>
+                        )}
+                    </View>
+
+                    {showReplies && <RepliesList parentId={item.id} replies={item.replies} comments={comments} />}
+
+                    {isReplying && (
+                        <TextInput 
+                            ref={replyInputRef}
+                            style={styles.replyInput}
+                            placeholder="Write a reply..."
+                            value={replyText}
+                            onChangeText={setReplyText}
+                            onSubmitEditing={async () => {
+                                if (replyText.trim()) {
+                                    await handleAddReply(item.id, replyText);
+                                    setReplyText('');  
+                                    setTimeout(() => replyInputRef.current?.focus(), 100);  
+                                }
+                            }}
+                        />
+                    )}
+
+                </View>
             </View>
-        </View>
-    );
+        );
+    }
+
+    const ReplyItem = ({ replyItem, commentItem }) => {
+        const [isReplying, setIsReplying] = useState(false);
+        const [replyText, setReplyText] = useState('');
+        const replyInputRef = useRef(null);
+
+        const handleReplyPress= () => {
+            setIsReplying(true);
+            setTimeout(() => replyInputRef.current?.focus(), 100);
+        }
+        return (
+            <View style={styles.commentContainer}>
+                <Image source={{ uri: replyItem.user.avatar }} style={styles.avatar} />
+                <View style={styles.commentContent}>
+                    <Text style={styles.commentUser}>{replyItem.user.name} replied to {commentItem?.user.name}</Text>
+                    <Text style={styles.commentTimestamp}>{replyItem.timestamp}</Text>
+                    <Text style={styles.commentText}>{replyItem.text}</Text>
+                
+
+                    <View style={styles.iconsContainer}>
+                        {/*like*/}
+                        <TouchableOpacity onPress={handleReplyPress}>
+                            <SvgUri uri={reply} width={14} height={14} />
+                        </TouchableOpacity>
+                    </View>
+
+                    {isReplying && (
+                        <TextInput 
+                            ref={replyInputRef}
+                            style={styles.replyInput}
+                            placeholder="Write a reply..."
+                            value={replyText}
+                            onChangeText={setReplyText}
+                            onSubmitEditing={async () => {
+                                if (replyText.trim()) {
+                                    await handleAddReply(replyItem.id, replyText);
+                                    setReplyText('');  
+                                    setTimeout(() => replyInputRef.current?.focus(), 100);  
+                                }
+                            }}
+                            
+                        />
+                    )}
+
+                    {replyItem.replies?.length > 0 && (
+                        <View style={styles.nestedRepliesContainer}>
+                            {replyItem.replies.map((nestedReply) => (
+                                <View key={nestedReply.id}>{renderReply({ replyItem: nestedReply, commentItem })}</View>
+                            ))}
+                        </View>
+                    )}
+                </View>
+            </View>
+
+        );
+    }
+
+    const RepliesList = ({ parentId, replies, comments }) => {
+        const [visibleReplies, setVisibleReplies] = useState(5);
+        const filteredReplies = replies.filter(reply => reply.parentId === parentId);
+        const commentItem = comments.find(comment => comment.id === parentId);
+
+        const handleShowMore = () => {
+            setVisibleReplies((prev) => (prev + 5));
+        };
+
+        return (
+            <View>
+                {filteredReplies.length > 0 && (
+                    <View style={styles.replyListContainer}>
+                        <FlatList
+                            data={filteredReplies.slice(0, visibleReplies)} 
+                            keyExtractor={(item) => String(item.id)}
+                            renderItem={({ item }) => <ReplyItem replyItem={item} commentItem={commentItem} />}
+                        />
+                        {filteredReplies.length > visibleReplies && (
+                        <TouchableOpacity onPress={handleShowMore}>
+                            <Text>-- Show more</Text>
+                        </TouchableOpacity>
+                    )}
+                    </View>
+                )}
+            </View>
+        );
+    };
 
     return (
         <Modal visible={visible} animationType="slide" transparent>
@@ -129,8 +322,8 @@ const CommentsModal = ({ post, visible, onClose }) => {
                 ): (
                     <FlatList
                         data={comments}
-                        renderItem={renderComment}
-                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => <CommentItem item={item} comments={comments} />}
+                        keyExtractor={(item, index) => String(item.id)}
                         contentContainerStyle={styles.commentsContainer}
                     />
                 )}
@@ -198,6 +391,7 @@ const styles = StyleSheet.create({
     commentUser: {
         fontSize: 16,
         fontFamily: 'Inter-Regular',
+        color: '#000000',
     },
     commentText: {
         fontSize: 15,
@@ -205,9 +399,22 @@ const styles = StyleSheet.create({
         marginVertical: 4,
         color: '#595959',
     },
+    iconsContainer: {
+        flexDirection: 'row',
+        alignItems:'center',
+        //justifyContent: 'center',
+        gap: 10,
+    },
+    totalReplies: {
+        marginTop: 3,
+    },
+
     commentTimestamp: {
         fontSize: 12,
         color: 'gray',
+    },
+    replyListContainer: {
+        marginTop: 10,
     },
     inputContainer: {
         flexDirection: 'row',
