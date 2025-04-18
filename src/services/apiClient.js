d9356a299cb461de2a6768073695f43ee131e384
Rui Zhao <rzhao7815@gmail.com>
2025-04-18 11:32:38 -0500

    Fix: enable global navigation on token refresh failure

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,23 +3,33 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { endpoints } from "./endPoints";
 import baseUrl from "../utils/api";
 import authService from "../services/authService";
+import { createNavigationContainerRef } from "@react-navigation/native";
 
+// Create a global navigation reference
+export const navigationRef = createNavigationContainerRef();
+
+// Create an instance of axios with the base URL
 const apiClient = axios.create({
     baseURL: baseUrl,
 });
 
+// This function logs the user out and reset the navigation
 const performLogout = async () => {
     try {
         await authService.logout();
-        navigation.reset({
-            index: 0,
-            routes: [{ name: 'Login' }],
-        });
+        if (navigationRef.isReady()) {
+            // Navigates the user back to the Login screen
+            navigationRef.reset({
+                index: 0,
+                routes: [{ name: 'Login' }],
+            });
+        }
     } catch (error) {
         console.error("Error logging out:", error);
     }
 };
 
+// Refresh the token when the access token has expired
 const refreshAccessToken = async () => {
     try {
         const refreshToken = await AsyncStorage.getItem("RefreshToken");
@@ -39,6 +49,7 @@ const refreshAccessToken = async () => {
     } catch (error) {
         console.error("Error Refreshing Token", error);
 
+        // If the refresh token is invalid/expired, Log the user out
         if(error.response && error.response.status === 401) {
             try {
                 console.error("Refresh token is expired or invalid");
@@ -52,6 +63,8 @@ const refreshAccessToken = async () => {
     }
 };
 
+// Axios Interceptor
+// Runs automatically on every response
 apiClient.interceptors.response.use(
     (response) => response, // Pass through successful responses
     async (error) => {
@@ -59,20 +72,21 @@ apiClient.interceptors.response.use(
             if (!error.config._retry) {
                 error.config._retry = true;
                 try {
+                    // If response has unauthorized status, try to get a new access token
                     const newAccessToken = await refreshAccessToken();
                     if (newAccessToken) {
-                        // Retry the original request with the new token
+                        // Update the original request with the new token
                         error.config.headers["Authorization"] = `Bearer ${newAccessToken}`;
-                        return apiClient.request(error.config);
+                        return apiClient.request(error.config); // Retry the request
                     }
                 } catch (refreshError) {
-                console.error("Failed to refresh token:", refreshError);
-                throw refreshError;
+                    console.error("Failed to refresh token:", refreshError);
+                    throw refreshError;
                 }
             }
         }
-        return Promise.reject(error); 
+        return Promise.reject(error); // Reject any other error.
     }
 );
-  
+
 export default apiClient;
